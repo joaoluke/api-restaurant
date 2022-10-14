8fc41be340044a1faad57dc635af35f5dc50cbaa
John Luke <joliveira@mwneo.com>
2022-10-14 00:33:20 -0300

    feat: add url production to cors

--- setup/settings.py
+++ setup/settings.py
@@ -122,6 +122,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:5500",
+    "https://layout-restaurant.vercel.app",
 ]
 
 # Static files (CSS, JavaScript, Images)
